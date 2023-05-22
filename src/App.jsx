08394d5fdfb366ebdbef38c88c0d73dfc28c3ac5
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-05-22 20:10:03 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./app.scss";
 import Blogs from "./components/blogs/Blogs";
+
 import Intro from "./components/intro/Intro";
+
 import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
 
